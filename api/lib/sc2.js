ab4cebbb24a6b1a97d4cc5d7b28e0763a1a1d0c8
Ye Hui <bonjovis1985@gmail.com>
2017-12-06 08:47:54 +0000

    use test to mork data,easy to dev

--- api/lib/sc2.js
+++ api/lib/sc2.js
@@ -1,4 +1,5 @@
-var request = require('request');
+const fetch = require('isomorphic-fetch');
+
 class SDKError extends Error {
   constructor(message, obj) {
     super(message);
@@ -50,26 +51,165 @@ SteemConnect.prototype.getLoginURL = function getLoginURL(state) {
   return loginURL;
 };
 
+SteemConnect.prototype.send = function send(route, method, body, cb) {
+  const url = `${this.options.baseURL}/api/${route}`;
+  const promise = fetch(url, {
+    method,
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      Authorization: this.options.accessToken,
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => {
+      const json = res.json();
+      // If the status is something other than 200 we need
+      // to reject the result since the request is not considered as a fail
+      if (res.status !== 200) {
+        return json.then(result => Promise.reject(new SDKError('sc2-sdk error', result)));
+      }
+      return json;
+    })
+    .then((res) => {
+      if (res.error) {
+        return Promise.reject(new SDKError('sc2-sdk error', res));
+      }
+      return res;
+    });
+
+  if (!cb) return promise;
+
+  return promise.then(res => cb(null, res)).catch(err => cb(err, null));
+};
+
+SteemConnect.prototype.broadcast = function broadcast(operations, cb) {
+  return this.send('broadcast', 'POST', { operations }, cb);
+};
+
 SteemConnect.prototype.me = function me(cb) {
   return this.send('me', 'POST', {}, cb);
 };
 
-SteemConnect.prototype.send = function send(route, method, body, cb) {
-    const url = `${this.options.baseURL}/api/${route}`;
-    var clientServerOptions = {
-        uri: url,
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': this.options.accessToken
-        },
-        body: JSON.stringify(body)
-    }
-    request(clientServerOptions, function (error, response) {
-        cb(error, response);
-        return response.body;
+SteemConnect.prototype.vote = function vote(voter, author, permlink, weight, cb) {
+  const params = {
+    voter,
+    author,
+    permlink,
+    weight,
+  };
+  return this.broadcast([['vote', params]], cb);
+};
+
+SteemConnect.prototype.comment = function comment(
+  parentAuthor,
+  parentPermlink,
+  author,
+  permlink,
+  title,
+  body,
+  jsonMetadata,
+  cb
+) {
+  const params = {
+    parent_author: parentAuthor,
+    parent_permlink: parentPermlink,
+    author,
+    permlink,
+    title,
+    body,
+    json_metadata: JSON.stringify(jsonMetadata),
+  };
+  return this.broadcast([['comment', params]], cb);
+};
+
+SteemConnect.prototype.reblog = function reblog(account, author, permlink, cb) {
+  const params = {
+    required_auths: [],
+    required_posting_auths: [account],
+    id: 'follow',
+    json: JSON.stringify([
+      'reblog',
+      {
+        account,
+        author,
+        permlink,
+      },
+    ]),
+  };
+  return this.broadcast([['custom_json', params]], cb);
+};
+
+SteemConnect.prototype.follow = function follow(follower, following, cb) {
+  const params = {
+    required_auths: [],
+    required_posting_auths: [follower],
+    id: 'follow',
+    json: JSON.stringify(['follow', { follower, following, what: ['blog'] }]),
+  };
+  return this.broadcast([['custom_json', params]], cb);
+};
+
+SteemConnect.prototype.unfollow = function unfollow(unfollower, unfollowing, cb) {
+  const params = {
+    required_auths: [],
+    required_posting_auths: [unfollower],
+    id: 'follow',
+    json: JSON.stringify(['follow', { follower: unfollower, following: unfollowing, what: [] }]),
+  };
+  return this.broadcast([['custom_json', params]], cb);
+};
+
+SteemConnect.prototype.ignore = function ignore(follower, following, cb) {
+  const params = {
+    required_auths: [],
+    required_posting_auths: [follower],
+    id: 'follow',
+    json: JSON.stringify(['follow', { follower, following, what: ['ignore'] }]),
+  };
+  return this.broadcast([['custom_json', params]], cb);
+};
+
+SteemConnect.prototype.claimRewardBalance = function claimRewardBalance(
+  account,
+  rewardSteem,
+  rewardSbd,
+  rewardVests,
+  cb
+) {
+  const params = {
+    account,
+    reward_steem: rewardSteem,
+    reward_sbd: rewardSbd,
+    reward_vests: rewardVests,
+  };
+  return this.broadcast([['claim_reward_balance', params]], cb);
+};
+
+SteemConnect.prototype.revokeToken = function revokeToken(cb) {
+  return this.send('oauth2/token/revoke', 'POST', { token: this.options.accessToken }, cb).then(
+    () => this.removeAccessToken()
+  );
+};
+
+SteemConnect.prototype.updateUserMetadata = function updateUserMetadata(metadata = {}, cb) {
+  return this.send('me', 'PUT', { user_metadata: metadata }, cb);
+};
+
+SteemConnect.prototype.sign = function sign(name, params, redirectUri) {
+  if (typeof name !== 'string' || typeof params !== 'object') {
+    return new SDKError('sc2-sdk error', {
+      error: 'invalid_request',
+      error_description: 'Request has an invalid format',
     });
-}
+  }
+  let url = `${this.options.baseURL}/sign/${name}?`;
+  url += Object.keys(params)
+    .map(key => `${key}=${encodeURIComponent(params[key])}`)
+    .join('&');
+  url += redirectUri ? `&redirect_uri=${encodeURIComponent(redirectUri)}` : '';
+  return url;
+};
 
 exports.Initialize = function Initialize(config) {
   const instance = new SteemConnect();
